0b5338083356f32e5a5d3905859c6f7f3396339a
jiajk <906154249@qq.com>
2020-07-29 18:13:24 +0800

    添加redis的常量设置文件

---
+++ gmall-manager-service/src/main/java/com/jsfund/gmall2019/manage/constant/ManageConst.java
@@ -0,0 +1,11 @@
+package com.jsfund.gmall2019.manage.constant;
+
+public class ManageConst {
+    public static final String SKUKEY_PREFIX="sku:";
+
+    public static final String SKUKEY_SUFFIX=":info";
+
+    public static final int SKUKEY_TIMEOUT=24*60*60;
+
+}
+
